87a2e610c62f7b51551f4a6ea93dff71aeb9271d
szulem <szulem@o2.pl>
2016-10-27 23:48:03 +0000

    15 pinów na stronie głównej

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,7 +6,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
   end
   
   def mypins
